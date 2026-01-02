46f07f5ec3fd97d4725cf102c47970ed03cf6431
Abhay  Dubey <146931800+abhaydubey200@users.noreply.github.com>
2026-01-02 23:42:10 +0530

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pandas as pd
+from core.database import get_connection
 
-st.title("🏠 DS Group Dashboard")
-st.success("Login successful")
+st.title("📜 Audit Logs")
+
+conn = get_connection()
+df = pd.read_sql("SELECT * FROM AUDIT_LOG ORDER BY CHANGE_TIME DESC", conn)
+st.dataframe(df, use_container_width=True)
